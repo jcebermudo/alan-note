797f094b006dda6178cd1ea54e247f971eb548c8
James <jamestech718@gmail.com>
2024-11-01 21:08:16 +0800

    aa

--- prisma.ts
+++ prisma.ts
@@ -4,12 +4,16 @@ import { fieldEncryptionExtension } from 'prisma-field-encryption'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+if (!process.env.PRISMA_FIELD_ENCRYPTION_KEY) {
+  throw new Error('PRISMA_FIELD_ENCRYPTION_KEY environment variable is required');
+}
+
 const prismaRaw =
     globalForPrisma.prisma || new PrismaClient({log: ['query']});
 
-export const prisma = prismaRaw.$extends(fieldEncryptionExtension(
-    {encryptionKey: process.env.PRISMA_FIELD_ENCRYPTION_KEY,}
-)) as PrismaClient;
+export const prisma = prismaRaw.$extends(fieldEncryptionExtension({
+  encryptionKey: process.env.PRISMA_FIELD_ENCRYPTION_KEY
+})) as PrismaClient;
   
 await migrate(prisma as PrismaClient)
 
