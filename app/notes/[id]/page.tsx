9ecc89432df113f8e1e75cba994e0a6003992905
James <jamestech718@gmail.com>
2024-11-04 20:52:58 +0800

    refactor NotePage component: update parameter handling to use props

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,8 +2,10 @@ import NoteSidebar from "@/components/NoteSidebar";
 import { prisma } from "@/prisma";
 import Tiptap from "@/components/Tiptap";
 
-export default async function NotePage({ params }: { params: { id: string } }) {
-  const id = await params;
+type Params = Promise<{ id: string }>;
+
+export default async function NotePage(props: { params: Params }) {
+  const id = await props.params;
   const idParams = id.id;
   const note = await prisma.note.findUnique({
     where: { id: idParams },
