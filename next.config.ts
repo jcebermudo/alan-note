438525738726ed18662779ea4375647f8aadcd26
James <jamestech718@gmail.com>
2024-11-04 20:32:55 +0800

    remove experimental serverComponentsExternalPackages configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: { serverComponentsExternalPackages: ["yjs"] },
 };
-
 export default nextConfig;
